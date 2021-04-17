f7c4f73dc3d8d71f1d52ee567428d7c49aba8369
Samantha Darris <darriss@remote1.cs.allegheny.edu>
2021-04-17 01:04:46 -0400

    lets see

--- pages/about.vue
+++ pages/about.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="about-page" class="page-wrapper about-page content-page">
     <site-hero
-      title="Traditional Marriage"
-      subtitle="Example About Page"
+      title="About the Creator"
       image="/uploads/IMG-4126.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h3>Samantha Darris</h3>
-          <p>
-            Samantha Darris is from New York City and attends ALlegheny College.
+          <h3><center>Samantha Darris</center></h3>
+
+          <p><center>
+            Samantha Darris is from New York City and attends Allegheny College.
             While she has always enjoyed computer science, throughout college she
             grew a passion for cultural anthropology, social justice, and transnational
             feminism. Her passion developed when she studied abroad in Morocco from
             August to December of 2019. This experience changed how she viewed international
             affairs, feminism, and marriage. She left Morocco with many questions,
             and looked for answers in this research project.
-          </p>
+          </center></p>
 
           <quote-card />
             <strong>Alias aut facere harum ipsam</strong>
