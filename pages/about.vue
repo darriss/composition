c159791ccf9a706d8151eb208dccb7b3b55b5890
Samantha Darris <darriss@remote1.cs.allegheny.edu>
2021-04-17 01:49:53 -0400

    image

--- pages/about.vue
+++ pages/about.vue
@@ -7,7 +7,7 @@
       <template v-slot:default>
         <div class="content">
           <h3><center>Samantha Darris</center></h3>
-
+            <img src="/uploads/IMG-4126.jpg"/>
           <p><center>
             Samantha Darris is from New York City and attends Allegheny College.
             While she has always enjoyed computer science, throughout college she
