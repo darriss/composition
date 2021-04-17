ae6b62fd7441a2d5e52e1a9863a2205b5325b161
Samantha Darris <darriss@remote1.cs.allegheny.edu>
2021-04-17 01:58:21 -0400

    no more form

--- pages/contact.vue
+++ pages/contact.vue
@@ -48,13 +48,6 @@
           </div>
         </div>
       </template>
-      <template v-slot:sidebar>
-        <h3 class="subtitle is-4">
-          Latest Posts
-        </h3>
-        <!-- Latest Posts -->
-        <posts-grid :per-row="1" :number="2" />
-      </template>
     </main-section>
   </div>
 </template>
