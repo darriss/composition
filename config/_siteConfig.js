5d1a27364c5b99f0a0ee2812f60aaf577a92d785
Samantha Darris <darriss@remote1.cs.allegheny.edu>
2021-04-23 01:33:02 -0400

    hmmm

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -24,7 +24,7 @@ export default {
     },
     {
       name: 'Resources',
-      link: '/resources'
+      link: '/contact'
     },
     {
       name: 'Github Repo',
--- pages/about.vue
+++ pages/about.vue
@@ -23,45 +23,6 @@
         </div>
 
                 <div class="tile is-ancestor">
-                  <div class="tile is-parent">
-                    <article class="tile is-child box">
-                      <p class="title">
-                        Cool Stuff We Do
-                      </p>
-                      <p class="subtitle">
-                        Sooo Cool
-                      </p>
-                      <figure class="image is-1by1">
-                        <opti-image width="500" height="500" />
-                      </figure>
-                    </article>
-                  </div>
-                  <div class="tile is-parent">
-                    <article class="tile is-child box">
-                      <p class="title">
-                        Other Cool Things
-                      </p>
-                      <p class="subtitle">
-                        Just as cool
-                      </p>
-                      <figure class="image is-1by1">
-                        <opti-image width="500" height="500" />
-                      </figure>
-                    </article>
-                  </div>
-                  <div class="tile is-parent">
-                    <article class="tile is-child box">
-                      <p class="title">
-                        All the cool
-                      </p>
-                      <p class="subtitle">
-                        that is all
-                      </p>
-                      <figure class="image is-1by1">
-                        <opti-image width="500" height="500" />
-                      </figure>
-                    </article>
-                  </div>
                 </div>
               </template>
 
@@ -69,6 +30,7 @@
                 <h3 class="subtitle is-4">
                   Latest Posts
                 </h3>
+                <image src="/uploads/IMG-4126.jpg">
                 <posts-grid :per-row="1" :number="2" />
               </template>
             </main-section>
